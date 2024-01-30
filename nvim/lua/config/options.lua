9cf51784b2c593a1c76733c15ea3360c5e3fb57e
Eduardo Paez <paezcuauhtemoc@gmail.com>
2024-01-29 19:21:26 -0700

    Merged

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,24 @@
+local opt = vim.opt
+opt.number = true
+opt.relativenumber = true
+opt.scrolloff = 7
+opt.backspace = "indent,eol,start"
+opt.cmdheight = 1
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- opt.cursorline = true
+
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+opt.fileformat = "unix"
+
+
+opt.termguicolors = true
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+-- opt.signcolumn = "yes" -- show sign column so that text doesn't shift
